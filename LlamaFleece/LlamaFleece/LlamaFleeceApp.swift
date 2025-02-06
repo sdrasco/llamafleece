efc920f05ffa207cd79643bcc37a910c68103d52
Steve Drasco <steve.drasco@gmail.com>
2025-02-06 12:56:09 +0000

    Initial commit wit Xcode project and repo files

---
+++ LlamaFleece/LlamaFleece/LlamaFleeceApp.swift
@@ -0,0 +1,17 @@
+//
+//  LlamaFleeceApp.swift
+//  LlamaFleece
+//
+//  Created by sdrasco on 06/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct LlamaFleeceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LlamaFleece/LlamaFleeceTests/LlamaFleeceTests.swift
@@ -0,0 +1,17 @@
+//
+//  LlamaFleeceTests.swift
+//  LlamaFleeceTests
+//
+//  Created by sdrasco on 06/02/2025.
+//
+
+import Testing
+@testable import LlamaFleece
+
+struct LlamaFleeceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
